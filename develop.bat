c6af138e00a21fb8d764ff7415a61348578c814a
srahman1601 <145057693+srahman1601@users.noreply.github.com>
2023-09-28 18:25:21 -0400

    Update develop.bat

--- develop.bat
+++ develop.bat
@@ -1,1 +1,19 @@
-New-Item fahed.txt
+This batch file details Windows 10, hardware, and networking configuration.
+
+TITLE My System Info
+
+ECHO Please wait... Checking system information.
+
+:: Section 1: Windows 10 information
+
+ECHO ==========================
+
+ECHO WINDOWS INFO
+
+ECHO ============================
+
+systeminfo | findstr /c:"OS Name"
+
+systeminfo | findstr /c:"OS Version"
+
+systeminfo | findstr /c:"System Type"
